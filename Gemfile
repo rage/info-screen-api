843c46d3250407508aa21909c66b73072c61b4b3
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-11-24 12:24:32 +0200

    Add coverage support

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sinatra-contrib', '1.4.2'
 gem 'sinatra-cross_origin', '0.3.2'
 gem 'json', '1.8.1'
 gem 'data_mapper', '1.2.0'
+gem 'nokogiri', '1.6.4.1'
 
 group :development do
 
--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require 'sinatra/reloader' if development?
 require 'sinatra/cross_origin'
 require 'json'
 require 'data_mapper'
+require 'open-uri'
+require 'nokogiri'
 
 require './configuration.rb'
 require './notification.rb'
@@ -17,6 +19,8 @@ configure do
   DataMapper.auto_upgrade!
 
 end
+
+# Notifications
 
 get '/notifications/?' do
 
@@ -34,5 +38,32 @@ post '/notifications/?' do
     ip: request.ip
 
   )
+
+end
+
+# Coverages
+
+get '/coverages/?' do
+
+  content_type :json
+
+  repositoryNodes = Nokogiri::HTML(open('https://coveralls.io/r/rage/')).css('.repoOverview')
+
+  repositories = repositoryNodes.inject([]) do | array, element |
+
+    headerNode = element.css('.repoChartInfo h1 a')
+    coverageElement = element.css('.repoChartInfo .coverageText')
+
+    array << {
+
+      name: "#{headerNode[0].text}/#{headerNode[1].text}",
+      coverage: coverageElement.text,
+      level: coverageElement.attr('class').text.split(' ')[0].split('-')[1]
+
+    }
+
+  end
+
+  repositories.to_json
 
 end
